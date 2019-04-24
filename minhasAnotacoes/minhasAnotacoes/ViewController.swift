a291acbe2b9620237844434d6e1d10b098940fbf
Fellipe Ricciardi Chiarello <fellipe.chiarello@gmail.com>
2019-04-24 09:00:59 -0300

    Salvando dados

--- minhasAnotacoes/minhasAnotacoes/ViewController.swift
+++ minhasAnotacoes/minhasAnotacoes/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textoCampo: UITextView!
+    
+    let chave = "minhaAnotacao"
+    
+    @IBAction func botaoSalvar(_ sender: Any) {
+        
+        if let texto = textoCampo.text{
+            
+            UserDefaults.standard.set( texto, forKey: chave)
+        }
+    }
+    
+    func recuperarAnotacao() -> String {
+        
+        if let textoRecuperado = UserDefaults.standard.object(forKey: chave){
+            return textoRecuperado as! String
+        }
+        
+        return ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textoCampo.text = recuperarAnotacao()
+        
     }
-
-
 }
-
