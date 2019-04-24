f06ab6ffa1ec968ad9f8a3905c784e8931294fdd
Fellipe Ricciardi Chiarello <fellipe.chiarello@gmail.com>
2019-04-24 08:44:10 -0300

    primeiro commit, main.storyboard

---
+++ minhasAnotacoes/minhasAnotacoes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  minhasAnotacoes
+//
+//  Created by Fellipe Ricciardi Chiarello on 4/24/19.
+//  Copyright © 2019 fchiarello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
